b25cf517d79d0c0e334a0e603f7da215c136889f
hawk2eyes <tvelden5@avans.nl>
2020-01-30 10:48:00 +0100

    initial push

---
+++ CFile1.c
@@ -0,0 +1,91 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** knipper.c
+**
+** Beschrijving:	Toggle even en oneven leds PORTD  
+** Target:			AVR mcu
+** Build:			avr-gcc -std=c99 -Wall -O3 -mmcu=atmega128 -D F_CPU=8000000UL -c knipper.c
+**					avr-gcc -g -mmcu=atmega128 -o knipper.elf knipper.o
+**					avr-objcopy -O ihex knipper.elf knipper.hex 
+**					or type 'make'
+** Author: 			dkroeske@gmail.com
+** -------------------------------------------------------------------------*/
+
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+/******************************************************************/
+void wait( int ms )
+/* 
+short:			Busy wait number of millisecs
+inputs:			int ms (Number of millisecs to busy wait)
+outputs:	
+notes:			Busy wait, not very accurate. Make sure (external)
+				clock value is set. This is used by _delay_ms inside
+				util/delay.h
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+/******************************************************************/
+int main( void )
+/* 
+short:			main() loop, entry point of executable
+inputs:			
+outputs:	
+notes:			Looping forever, flipping bits on PORTD
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	wipe(); // turns all leds off
+	
+	PINC = 0x01;
+	PORTD = 0x80;
+	
+	while (1)
+	{
+		if (PINC & 0x80)
+		{
+			for (int i = 1; i < 8 ; i++)
+			{
+				PORTD = i;
+				wait(50);
+			}
+		}
+		else
+		{
+			PORTD = 0x00;				// write 0 to all the bits of PortD
+		}		
+	}
+
+	return 1;
+}
+
+void wipe ()
+{
+	DDRA = 0b11111111;
+	DDRB = 0b11111111;
+	DDRC = 0b11111111;
+	DDRD = 0b11111111;
+	DDRE = 0b11111111;
+	DDRF = 0b11111111;
+	DDRG = 0b11111111;
+	
+	PORTA = 0x00;
+	PORTB = 0x00;
+	PORTC = 0x00;
+	PORTD = 0x00;
+	PORTE = 0x00;
+	PORTF = 0x00;
+	PORTG = 0x00;
+}

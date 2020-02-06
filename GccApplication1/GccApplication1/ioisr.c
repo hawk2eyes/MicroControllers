1f89b9d4c56d071b61b934086aeb4d01e49489e7
Jelmer <Jelmer.surewaard@gmail.com>
2020-02-06 10:25:45 +0100

    [ADD] Week 2 Opdracht B1

--- GccApplication1/GccApplication1/ioisr.c
+++ GccApplication1/GccApplication1/ioisr.c
@@ -28,56 +28,5 @@
 #include <avr/interrupt.h>
 
 
-ISR( INT0_vect )
-/* 
-short:			ISR INT0
-inputs:			
-outputs:	
-notes:			Set PORTD.5
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-    PORTD |= (1<<5);		
-}
 
-/******************************************************************/
-ISR( INT1_vect )
-/* 
-short:			ISR INT1
-inputs:			
-outputs:	
-notes:			Clear PORTD.5
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-    PORTD &= ~(1<<5);		
-}
 
-/******************************************************************/
-void ioisrMain()
-/* 
-short:			main() loop, entry point of executable
-inputs:			
-outputs:	
-notes:			Slow background task after init ISR
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-	// Init I/O
-	DDRD = 0xF0;			// PORTD(7:4) output, PORTD(3:0) input	
-
-	// Init Interrupt hardware
-	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
-	EIMSK |= 0x03;			// Enable INT1 & INT0
-	
-	// Enable global interrupt system
-	//SREG = 0x80;			// Of direct via SREG of via wrapper
-	sei();				
-
-	while (1)
-	{
-		PORTD ^= (1<<7);	// Toggle PORTD.7
-		wait( 500 );								
-	}
-
-}
